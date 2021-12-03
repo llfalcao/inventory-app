d832edcab67c0f39cb84c2de9b00871d0543a7eb
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-12-03 18:45:53 -0300

    Create Category model

---
+++ models/category.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true, maxLength: 100 },
+});
+
+// TODO: Virtual URL
+CategorySchema.virtual('url').get(() => `/inventory/category/${this._id}`);
+
+module.exports = mongoose.model('Category', CategorySchema);
